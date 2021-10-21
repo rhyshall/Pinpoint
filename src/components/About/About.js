7b80a2b1972a3020ec9b551f296d0816a6df15f6
Hall <Hall_R@homehardware.ca>
2021-10-21 17:32:37 -0400

    Added About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,8 +6,67 @@ class About extends Component
   render()
   {
     return(<div className="--about-container">
-             <h1>About</h1>
-           </div>)
+             <div className="--about-header-content">
+               <h2>About Us</h2>
+             </div>
+             
+             <div className="about-features">
+               <div className="--about-feature --about-img-left">
+                 <img className="--about-img"
+                      src="navigate.png"
+                      alt="Navigate"></img>
+                 <p>PinPoint Cities is all about mixing fun with education in this turn-based, two-player
+                    game. One "target" city is drawn and placed on the map with other "decoy" cities. The first player to "pinpoint" the target city wins the round.</p>
+               </div>
+  
+               <div className="--about-feature --about-img-right">
+                 <p>Customize your experience by choosing your desired country, city range (eg. 100 most  
+                    populous), spawn count (number of cities that appear on the map, including the target city) and max score (score to win).</p>
+                    <img className="--about-img"
+                         src="choose.png"
+                         alt="Choice"></img>
+               </div>
+  
+               <div className="--about-feature --about-img-left">
+                 <img className="--about-img"
+                      src="player.png"
+                      alt="Player"></img>
+                 <p>Go head-to-head against friends and family in local PvP mode or face anyone across the
+                    world in network PvP mode.</p>
+               </div>
+  
+               <div className="--about-feature --about-img-right">
+                 <p>Build your skills by practising against AI bots in easy, medium or hard mode.</p>
+                 <img className="--about-robot-img"
+                      src="robot.png"
+                      alt="Robot"></img>
+               </div>
+  
+               <div className="--about-feature --about-img-left">
+                 <img className="--about-img"
+                      src="brain.png"
+                      alt="Brain"></img>
+                 <p>Sharpen your knowledge in cities each time you play. In addition, brain games like 
+                    PinPoint Cities generally help improve memory capabilities and spatial awareness.</p>
+               </div>  
+  
+               <div className="--about-feature --about-img-right">
+                 <p>Take a trip to the Glossary page now and then to become more familiar with each 
+                    city.</p>
+                 <img className="--about-img"
+                      src="study.png"
+                      alt="Study"></img>
+               </div>  
+
+               <div className="--about-feature --about-img-left">
+                 <img className="--about-img"
+                      src="communicate.png"
+                      alt="Communicate"></img>
+                 <p>We are under the radar. If you enjoy your experience, please share to help PinPoint
+                    Cities become more prominent!</p>
+               </div> 
+             </div>                                   
+           </div>);
   }
 }
 
