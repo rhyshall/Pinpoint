37e05c55758cc75ea997482ae5e69808dbc2d860
Brett Hall <brett.hall.1993@gmail.com>
2022-05-26 12:43:44 -0400

    layout fixes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,9 +3,23 @@ import './Home.css';
 
 class Home extends Component
 {
+  constructor(props) {
+    super(props);
+    this.playSound = this.playSound.bind(this);
+  }
+
+  playSound() {
+    console.log('playSound()');
+    const path = require('../../sounds/game-over.mp3').default;
+    const audio = new Audio(path);
+    audio.load();
+    audio.play().then(() => console.log('audio playing')).catch(err => console.log(err));
+  }
+
   render()
   {
     return(<div className="--home-container">
+            <button onClick={this.playSound}>Play Sound</button>
            </div>);
   }
 }
